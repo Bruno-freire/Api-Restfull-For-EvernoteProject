77730c162efc4442a192afde536a036e4a2cca84
Bruno Freire <brunofreire1717@gmail.com>
2023-06-15 10:11:34 -0500

    vercel

--- config/database.js
+++ config/database.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const cluster = process.env.DB_CLUSTER;
+const dbName = process.env.DB_NAME;
+
+const url = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://0.0.0.0/javascriptNote', { useNewUrlParser: true, useUnifiedTopology: true,   autoIndex: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,   autoIndex: true})
 .then(() => console.log('Conectado ao mongoDB'))
 .catch((err) => {console.log(err.message)})
