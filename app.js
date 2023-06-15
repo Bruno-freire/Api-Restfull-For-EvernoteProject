aadfb3af48818ab0707f938212c5eb1cf66278ff
Bruno Freire <brunofreire1717@gmail.com>
2023-06-15 12:46:35 -0500

    removeVercel

--- app.js
+++ app.js
@@ -15,15 +15,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.get('/', (req,res) => {
-  return res.json("Bem vindos")
-})
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
 
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+module.exports = app;
